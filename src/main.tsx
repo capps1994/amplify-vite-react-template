e8809be118b976e862845f160fdb9af3bcdf9733
Christopher Caplan <ccaplan@hotmail.co.uk>
2025-06-20 22:48:04 +0100

    added api

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,29 @@ import App from "./App.tsx";
 import "./index.css";
 import '@aws-amplify/ui-react/styles.css';
 import { Amplify } from "aws-amplify";
+import { parseAmplifyConfig } from "aws-amplify/utils";
 import outputs from "../amplify_outputs.json";
 
+// const amplifyConfig = parseAmplifyConfig(outputs);
+
+// Amplify.configure(
+//   {
+//     ...amplifyConfig,
+//     API: {
+//       ...amplifyConfig.API,
+//       REST: outputs.custom.API,
+//     },
+//   },
+//   {
+//     API: {
+//       REST: {
+//         retryStrategy: {
+//           strategy: 'no-retry' // Overrides default retry strategy
+//         },
+//       }
+//     },
+//   }
+// );
 Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
